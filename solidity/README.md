9b4384ee61aecd6e7b63515b890c42fabbf719da
David Echelberger <david.echelberger@kaleido.io>
2022-01-28 10:02:47 -0500

    [use-method-abis] solidity hardhat
    Signed-off-by: David Echelberger <david.echelberger@kaleido.io>

---
+++ solidity/README.md
@@ -0,0 +1,19 @@
+# ERC20 and ERC 721 Solidity
+
+## Overview
+
+Ethereum smart contract implementations of ERC20 and ERC721 connectors
+
+## Usage
+
+### Compile
+
+```shell
+npm run compile
+```
+
+#### Test
+
+```shell
+npm run test
+```
---
+++ solidity/hardhat.config.ts
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv';
+
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+
+dotenv.config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  // solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.0',
+      },
+    ],
+  },
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
